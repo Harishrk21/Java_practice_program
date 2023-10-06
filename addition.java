292b6ad574ee13b1b605a22b9deca27e62aa967f
Harish Kumar <harishradhakrishnan2001@gmail.com>
2023-10-06 14:42:40 +0530

    added print stmt

--- addition.java
+++ addition.java
@@ -2,6 +2,7 @@ public class addition {
     public static void main(String[] args) {
         int a=10,b=10,c;
         c=a+b;
+        System.out.println("sample ");
         System.out.println("The addition of two numbers is:"+c);
     }
 }
