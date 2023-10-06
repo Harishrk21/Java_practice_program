ebbb80df3490d2c1728f6ec3ec88f6afb89ac49f
Harish Kumar <harishradhakrishnan2001@gmail.com>
2023-10-06 14:39:46 +0530

    My first commit

---
+++ addition.java
@@ -0,0 +1,7 @@
+public class addition {
+    public static void main(String[] args) {
+        int a=10,b=10,c;
+        c=a+b;
+        System.out.println("The addition of two numbers is:"+c);
+    }
+}
