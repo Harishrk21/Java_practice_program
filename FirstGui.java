f80244c8f272481ae30530d741d1b14ad3051aa0
Harish Kumar R <harishradhakrishnan2001@gmail.com>
2024-02-09 15:41:31 +0530

    FIX: changes

---
+++ FirstGui.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+public class FirstGui {
+    public static void main(String[] args) {
+        abc obj =new abc();
+        obj.setVisible(true);
+        obj.setSize(400,400);
+    }
+}
+class abc extends JFrame
+{
+    public abc()
+    {
+        setLayout(new FlowLayout());
+        JLabel a = new JLabel("kundhaani");
+        add(a);
+        JLabel b = new JLabel("kiruku");
+        add(b);
+    }
+}
